6a00d4115237b0e300b5253592c2010c63a24ded
Thutmose <Thutmose@users.noreply.github.com>
2022-09-18 15:13:36 -0400

    sync from pokecube

--- src/main/java/thut/core/client/render/model/parts/Mesh.java
+++ src/main/java/thut/core/client/render/model/parts/Mesh.java
@@ -7,6 +7,7 @@ import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.blaze3d.vertex.VertexFormat.Mode;
 import com.mojang.math.Matrix3f;
 import com.mojang.math.Matrix4f;
+import com.mojang.math.Vector3f;
 import com.mojang.math.Vector4f;
 
 import thut.api.maths.vecmath.Vec3f;
@@ -38,6 +39,9 @@ public abstract class Mesh
 
     public final Mode vertexMode;
 
+    Vertex min = new Vertex(0, 0);
+    Vertex max = new Vertex(0, 0);
+
     final int iter;
 
     static double sum;
@@ -62,6 +66,9 @@ public abstract class Mesh
 
         vertexMode = GL_FORMAT == GL11.GL_TRIANGLES ? Mode.TRIANGLES : Mode.QUADS;
 
+        Vec3f mins = new Vec3f(Float.MAX_VALUE, Float.MAX_VALUE, Float.MAX_VALUE);
+        Vec3f maxs = new Vec3f(Float.MIN_VALUE, Float.MIN_VALUE, Float.MIN_VALUE);
+
         // Calculate the normals for each triangle.
         for (int i = 0; i < this.order.length; i += iter)
         {
@@ -73,9 +80,42 @@ public abstract class Mesh
             vertex = this.vertices[this.order[i + 2]];
             v3 = new Vec3f(vertex.x, vertex.y, vertex.z);
 
+            if (v1.x < mins.x) mins.x = v1.x;
+            if (v1.y < mins.y) mins.y = v1.y;
+            if (v1.z < mins.z) mins.z = v1.z;
+            if (v2.x < mins.x) mins.x = v2.x;
+            if (v2.y < mins.y) mins.y = v2.y;
+            if (v2.z < mins.z) mins.z = v2.z;
+            if (v3.x < mins.x) mins.x = v3.x;
+            if (v3.y < mins.y) mins.y = v3.y;
+            if (v3.z < mins.z) mins.z = v3.z;
+
+            if (v1.x > maxs.x) maxs.x = v1.x;
+            if (v1.y > maxs.y) maxs.y = v1.y;
+            if (v1.z > maxs.z) maxs.z = v1.z;
+            if (v2.x > maxs.x) maxs.x = v2.x;
+            if (v2.y > maxs.y) maxs.y = v2.y;
+            if (v2.z > maxs.z) maxs.z = v2.z;
+            if (v3.x > maxs.x) maxs.x = v3.x;
+            if (v3.y > maxs.y) maxs.y = v3.y;
+            if (v3.z > maxs.z) maxs.z = v3.z;
+
             centre.add(v1.x, v1.y, v1.z, 0);
             centre.add(v2.x, v2.y, v2.z, 0);
             centre.add(v3.x, v3.y, v3.z, 0);
+            if (iter == 4)
+            {
+                vertex = this.vertices[this.order[i + 3]];
+                Vec3f v4 = new Vec3f(vertex.x, vertex.y, vertex.z);
+                centre.add(v4.x, v4.y, v4.z, 0);
+                if (v4.x < mins.x) mins.x = v4.x;
+                if (v4.y < mins.y) mins.y = v4.y;
+                if (v4.z < mins.z) mins.z = v4.z;
+
+                if (v4.x > maxs.x) maxs.x = v4.x;
+                if (v4.y > maxs.y) maxs.y = v4.y;
+                if (v4.z > maxs.z) maxs.z = v4.z;
+            }
 
             final Vec3f a = new Vec3f(v2);
             a.sub(v1);
@@ -101,11 +141,21 @@ public abstract class Mesh
 
         centre.mul(1.0f / order.length);
 
+        min.set(mins);
+        max.set(maxs);
+
         // Initialize a "default" material for us
         this.material = new Material("auto:" + this.name);
     }
 
-    private final com.mojang.math.Vector3f dummy3 = new com.mojang.math.Vector3f();
+    public void scale(float scale)
+    {
+        for (Vertex v : this.vertices) v.scale(scale);
+    }
+
+    private final Vector3f dummy3 = new Vector3f();
+    private final Vector3f dummy_1 = new Vector3f();
+    private final Vector3f dummy_2 = new Vector3f();
     private final Vector4f dummy4 = new Vector4f();
     private final TextureCoordinate dummyTex = new TextureCoordinate(0, 0);
 
@@ -141,20 +191,25 @@ public abstract class Mesh
 
         float x, y, z, nx, ny, nz, u, v;
 
-        com.mojang.math.Vector3f camera_view = com.mojang.math.Vector3f.ZP;
+        dummy_1.set(min.x, min.y, min.z);
+        dummy_2.set(max.x, max.y, max.z);
 
-        boolean cull = material.cull && alpha >= 1 && !material.transluscent;
+        dummy_2.sub(dummy_1);
+        dummy_1.set(centre.x(), centre.y(), centre.z());
+        dummy_2.sub(dummy_1);
 
-        if (cull)
-        {
-            dp.set(centre.x(), centre.y(), centre.z(), 1);
-            dp.transform(pos);
-            double dr2 = Math.abs(dp.dot(METRIC));
-            if (dr2 < CULLTHRESHOLD || dr2 > 6e2)
-            {
-                cull = false;
-            }
-        }
+        dp.set(dummy_2.x(), dummy_2.y(), dummy_2.z(), 1);
+
+        double dr2_us = Math.abs(dp.dot(METRIC));
+
+        dp.set(centre.x(), centre.y(), centre.z(), 1);
+        dp.transform(pos);
+
+        double dr2_camera = Math.abs(dp.dot(METRIC));
+
+        boolean size_cull = Mesh.CULLTHRESHOLD < Double.MAX_VALUE && dr2_camera < 4e2 && dr2_us < dr2_camera / 50;
+
+        if (size_cull) return;
 
         // Loop over this rather than the array directly, so that we can skip by
         // more than 1 if culling.
@@ -172,21 +227,6 @@ public abstract class Mesh
 
             dn.set(nx, ny, nz);
             dn.transform(norms);
-
-            // flat vs smooth have slightly different cull criteria.
-            // Smooth must only cull on the first normal, otherwise we add a
-            // messed up thing to the render. I guess later we can check the
-            // other normals instead, but for now only checking first, and then
-            // a -0.2 for the threshold works.
-            final boolean tryCull = cull
-                    && (flat && dn.dot(camera_view) < 0.0 || i0 % iter == 0 && dn.dot(camera_view) < -0.2);
-
-            if (tryCull)
-            {
-                // Manually iterate a few to skip the entire face.
-                i0 += iter - 1;
-                continue;
-            }
 
             // Next we can pull out the coordinates if not culled.
             if (this.hasTextures) textureCoordinate = this.textureCoordinates[i];
